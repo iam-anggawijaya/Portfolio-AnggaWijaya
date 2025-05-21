cd4110513f4c09264049d3765c25ceea5bcd2411
iam-anggawijaya <imadeanggawijaya@gmail.com>
2025-05-21 15:17:19 +0800

    updated 21 May 2025

--- src/components/ui/About/YearsExperience.tsx
+++ src/components/ui/About/YearsExperience.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 
 import { cn } from '@/lib/utils';
 
-import FadeInWhenVisible from '../FadeInWhenVisible';
-
 type YearsExperienceProps = {
   title?: string;
   className?: string;
